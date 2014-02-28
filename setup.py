9a16a7586985b4f989e601863558dc4f08c7d909
p1c2u <maciag.artur@gmail.com>
2014-02-28 19:27:07 +0100

    setup file update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wykop-sdk',
-    version='0.1.2',
+    version='0.2.0',
     description='Client library for Wykop API',
     
     long_description=open("README.rst").read(),
